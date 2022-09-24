6f75487cbc4951878baf65ae4a6f34bcd88f71c4
sHammer4 <90473670+sHammer4@users.noreply.github.com>
2022-09-23 23:05:53 -0600

    Added assignment 1 starter files

---
+++ assignment-1-start/js/dom/weather.js
@@ -0,0 +1,16 @@
+/*
+HTML Structure 
+
+<div class="mt-2 card" >
+  <div class="card-body">
+    <h5 class="card-title">CITY_NAME_HERE, COUNTRY_CODE_HERE</h5>
+    <h6 class="card-subtitle mb-2 text-muted">CURRENT_WEATHER_DEGREES_HERE</h6>
+    <p class="card-text">WEATHER_DESCRIPTION_HERE</p>
+  </div>
+</div>
+
+*/
+
+// renderWeather function
+
+// export the renderWeather function
