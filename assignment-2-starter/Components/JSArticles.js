c2e1eac66ad168209e652f000a49d9e3a1460620
Susanto Hammer <shammer4@nait.ca>
2022-10-12 22:08:57 -0600

    Done done diddity done

--- assignment-2-starter/Components/JSArticles.js
+++ assignment-2-starter/Components/JSArticles.js
@@ -1,12 +1,12 @@
 import { NEXT_JS_COOL_SITES } from "../nextjsSiteList";
 import { List, ListItem, Typography } from "@mui/material";
 
-export default function NextJsCoolSites() {
+export default function JSArticles() {
     return <List>
-        {NEXT_JS_COOL_SITES.map((name, url, index) => {
+        {NEXT_JS_COOL_SITES.map((instance, index) => {
             return <ListItem key={index}>
-                <a href={url}>
-                    <Typography>{name}</Typography>
+                <a href={instance.url}>
+                    <Typography color="primary">{instance.name}</Typography>
                 </a>
             </ListItem>
         })}
--- assignment-2-starter/pages/index.js
+++ assignment-2-starter/pages/index.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography';
-import NextJsCoolSites from '../Components/JSArticles';
+import AppBar from '@mui/material/AppBar';
+import JSArticles from '../Components/JSArticles';
 
 export default function Home() {
   return (
@@ -15,14 +16,20 @@ export default function Home() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       </Head>
       <main>
+        <AppBar position="static" title="Assignment 2">
+          <Typography sx={{ m: 2 }}>
+            Assignment 2
+          </Typography>
+        </AppBar>
         <Container>
+          
           <Typography variant="h2" component="h2" color="primary" align="center">
             Assignment 2 - React Components and Styling
           </Typography>
           <Typography color="secondary" align="center">
           Here's a list of awesome resources that you can use
           </Typography>
-          <NextJsCoolSites/>
+          <JSArticles />
         </Container>
       </main>
     </div>
