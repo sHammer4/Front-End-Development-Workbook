a51666aa853a5523c0d119f8a4a14da673f94b88
sHammer4 <90473670+sHammer4@users.noreply.github.com>
2022-11-01 11:34:35 -0600

    agadhrdgareg

---
+++ 5 React/reviews-app-with-lifecycle-START/reviews-rest-app/components/AdaptationReviewCard.js
@@ -0,0 +1,50 @@
+import Avatar from '@mui/material/Avatar';
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+
+import Typography from '@mui/material/Typography';
+
+import DeleteIcon from '@mui/icons-material/Delete'
+import IconButton from '@mui/material/IconButton';
+
+import { deleteReview } from '../utils/api/reviews';
+
+export default function AdaptationReviewCard(props) {
+  const deleteReviewHandler = (reviewId) => {
+    deleteReview(reviewId).then((data) => {
+      console.log(`Deleted ${reviewId}`)
+      props.deleteReviewFromState(reviewId)
+    })
+  }
+  
+  return <Card sx={{mt: 2 }}>
+    <CardHeader
+      avatar={
+        <Avatar sx={{ bgcolor: 'blue' }} aria-label="recipe">
+          {props.rating}
+        </Avatar>
+      }
+
+      action={
+        <IconButton onClick={() => {deleteReviewHandler(props.id)}}>
+          <DeleteIcon />
+        </IconButton>
+      }
+      
+      title={
+        <Typography variant="body2" color="text.secondary">
+          {props.title}
+        </Typography>
+      }
+      
+    />
+    <CardContent>
+      <Typography variant="body2" color="text.secondary">
+        {props.comment}
+      </Typography>
+    </CardContent>
+  </Card> 
+}
